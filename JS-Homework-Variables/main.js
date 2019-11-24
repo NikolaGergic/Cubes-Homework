834794ea7485409ec3646183873046157affe31c
NikolaGergic <ngergic@gmail.com>
2019-11-24 10:01:46 +0100

    Adding files for JS-variables homework

---
+++ JS-Homework-Variables/main.js
@@ -0,0 +1,37 @@
+// TASK 1 
+var Peter,
+	Jim;
+
+Peter = 1000;
+Jim = 5000;
+
+console.log ( Peter > Jim);
+
+// TASK 2
+
+var John,
+	Mike;
+
+John = 38;
+Mike = (John + 8) / 2;
+console.log(Mike);
+
+// TASK 3
+
+var firstFlock,
+	secondFlock,
+	move,
+	left;
+
+firstFlock = 78;
+secondFlock = 57;
+move = 12;
+left = 4;
+
+firstFlock -= move;
+secondFlock += move;
+secondFlock -= left;
+
+console.log (firstFlock, secondFlock);
+console.log (firstFlock >= secondFlock);
+
